986e1e18a99fb7cb6fcaab348e06f9771b90c30e
karkro <karkro@karkro>
2018-12-27 21:42:28 +0100

    Java Threads

---
+++ JavaCourseDB/src/CheckSystemTime.java
@@ -0,0 +1,23 @@
+// JavaLesson18
+
+import java.util.*;
+import java.text.DateFormat;
+
+public class CheckSystemTime implements Runnable {
+
+	public void run() {
+		
+		Date rightNow;
+		Locale currentLocale;
+		DateFormat timeFormatter;
+		String timeOutput;
+		
+		rightNow = new Date();
+		currentLocale = new Locale("pl");
+		
+		timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
+		timeOutput = timeFormatter.format(rightNow);
+		
+		System.out.println("Time: " + timeOutput);
+	}
+}
