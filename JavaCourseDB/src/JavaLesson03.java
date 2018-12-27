9cca57b852f60c20ba4d3271cfe005004f0e632d
karkro <karkro@karkro>
2018-12-27 17:35:12 +0100

    Random, relational and logical operators, switch statements

---
+++ JavaCourseDB/src/JavaLesson03.java
@@ -0,0 +1,121 @@
+
+public class JavaLesson03 {
+
+	public static void main(String[] args) {
+		
+		// Creates a random number between 0 and 50
+		int randomNumber = (int) (Math.random() * 50);
+		System.out.println("The random number: " + randomNumber);
+		/*
+		 * Relational operators:
+		 * Java has 6 relational operators
+		 *  > : Greater than
+		 *  < : Less than
+		 *  == : Equal to
+		 *  != : Not equal to
+		 *  >= : Greater than or equal to
+		 *  <= : Less than or equal to
+		 */
+		
+		// If randomNumber is less than 25, execute the code between {} and then stop checking
+		if (randomNumber < 25) {
+			System.out.println("The random number is less than 25.");
+		}
+		
+		// If randomNumber  wasn't less than 25, then check if it's greater than it.
+		// If so, execute the code between {} and then stop checking.
+		else if (randomNumber > 25) {
+			System.out.println("The random number is greater than 25.");
+		}
+		
+		// Check if randomNumber equal 25
+		else if (randomNumber == 25) {
+			System.out.println("The random number is equal to 25.");
+		}
+		
+		// Check if randomNumber is not equal to 15
+		else if (randomNumber != 15) {
+			System.out.println("The random number is not equal to 15.");
+		}
+		
+		// Check if randomNumber is less than or equal to 25
+		else if (randomNumber <= 25) {
+			System.out.println("The random number is less than or equal to 25.");
+		}
+		
+		// Check if randomNumber is greater than or equal to 25
+		else if (randomNumber >= 25) {
+			System.out.println("The random number is greater than or equal to 25.");
+		}
+		
+		// If none of the above were correct print out the random number
+		else {
+			System.out.println("The random number is " + randomNumber);
+		}
+		
+		/* Logical Operators:
+		 * Java has 6 logical operators
+		 * ! : Converts the boolean value to its right to its opposite form ie. true to false
+		 * & : Returns true if boolean value on the right and left are both true (Always evaluates both boolean values)
+		 * && : Returns true if boolean value on the right and left are both true (Stops evaluating after first false)
+		 * | : Returns true if either boolean value on the right or left are true (Always evaluates both boolean values)
+		 * || : Returns true if either boolean value on the right or left are true (Stops evaluating after first true)
+		 * ^ : Returns true if there is 1 true and 1 false boolean value on the right or left
+		 */
+		
+		if (!(false)) {
+			System.out.println("I turned false into true.");
+		}
+		
+		if ((false) && (true)) {
+			System.out.println("\nBoth are true.");
+		}
+		
+		// There is also a & logical operator it checks the second boolean result even if the first comes back false.
+		if ((true) ^ (true)) {
+			System.out.println("\nAt least 1 is true");
+		}
+
+        // There is also a | logical operator it checks the second boolean result even if the first comes back true
+		if ((true) ^ (false)) {
+		System.out.println("\n1 is true and the other false");
+        }
+		
+        int valueOne = 1;
+        int valueTwo = 2;
+        
+		// The Conditional or Ternary Operator assigns one or another value based on a condition
+		// If true valueOne is assigned to biggestValue. If not valueTwo is assigned
+		int biggestValue = (valueOne > valueTwo) ? valueOne : valueTwo;
+		System.out.println(biggestValue + " is the biggest\n");
+		
+		char theGrade = 'B';
+		/* When you have a limited number of possible values a switch statement makes sense
+		 * The switch statement checks the value of theGrade to the values that follow case
+		 * If it matches it executes the code between {} and then break ends the switch statement
+		 * default code is executed if there are no matches
+		 * You are not required to use the break or default statements
+		 * The expression must be an int, short, byte, or char
+		 */ 
+		switch (theGrade) {
+		case 'A':
+			System.out.println("Great Job");
+			break; // Ends the switch statement
+		case 'b': // You can use multiple case statement in a row
+		case 'B':
+			System.out.println("Good Job, get an A next time");
+			break;
+		case 'C':
+			System.out.println("Ok, but you can do better");
+			break;
+		case 'D':
+			System.out.println("You must work harder");
+			break;
+		default:
+			System.out.println("You failed");
+			break;
+		}
+	
+	}
+
+}
