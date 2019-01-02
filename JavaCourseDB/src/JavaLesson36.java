0c6ed8b9c6501ca8cf3cfd66be8c7c5c5004ede7
karkro <karkro@karkro>
2019-01-02 22:00:26 +0100

    How to display database query results into a Java JTable - start.

---
+++ JavaCourseDB/src/JavaLesson36.java
@@ -0,0 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.font.*;
+import java.awt.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+
+
+public class JavaLesson36 {
+
+}
