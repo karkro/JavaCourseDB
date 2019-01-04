10c36dc41fa01ee8ccc68f49f707dc8368182429
karkro <karkro@karkro>
2019-01-04 20:49:12 +0100

    How to display database query results into a Java JTable - cd.

--- JavaCourseDB/src/JavaLesson36.java
+++ JavaCourseDB/src/JavaLesson36.java
@@ -1,11 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.font.*;
-import java.awt.sql.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
+import java.sql.*;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,18 +13,36 @@ import javax.swing.table.TableColumn;
 
 public class JavaLesson36 {
 
+	// Used to hold the column data for each player
 	static Object[][] databaseInfo;
+	
+	// The column titles for the JTable
 	static Object[] columns = {"Year", "PlayerID", "Name", "TTRC", "Team", "Salary", "CPR", "POS"};
+	
+	// A ResultSet contains a table of data filled with the result of the query
 	static ResultSet rows;
+	
+	// ResultSetMetaData contains information on the data returned by the query
 	static ResultSetMetaData metaData;
+	
+	// DefaultTableModel defines the methods JTable will use
+	// I'm overriding the getColumnClass
 	static DefaultTableModel dTableModel = new DefaultTableModel(databaseInfo, columns) {
 		public Class getColumnClass(int column) {
 			Class returnValue;
+			
+			// Verifying that the column exists (index > 0 && index < number of columns)
 			if ((column >= 0) && (column < getColumnCount())) {
 				returnValue = getValueAt(0, column).getClass();
+			} else {
+				
+				// Returns the class for the item in the column
+				returnValue = Object.class;
 			}
+			
+			return returnValue;
 		}
-	}
+	};
 	
 	public static void main(String[] args) {
 		
@@ -39,19 +52,34 @@ public class JavaLesson36 {
 		
 		try {
 		
+			// The driver allows you to query the database with Java
+			// forName dynamically loads the class for you
 			Class.forName("com.mysql.jdbc.Driver");
+			
+			// DriverManager is used to handle a set of JDBC drivers
+			// getConnection establishes a connection to the database
+			// You must also pass the userid and password for the database
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/lahman591", root, karolkrol83);
+			
+			// Statement objects executes a SQL query
+			// createStatement returns a Statement object
 			Statement sqlState = conn.createStatement();
 			
+			// This is the query I'm sending to the database
 			String selectStuff = "select b.yearID, b.playerID, m.nameLast, m.nameFirst, b.teamID AS Team, s.salary AS Salary, f.POS AS POS FROM Batting b, Master m, Salaries s, TOTBYR t, Fielding f WHERE b.playerID = m.playerID AND t.playerID = m.playerID AND t.yearID = 2010 AND b.yearID = t.yearID AND s.playerID = b.playerID AND s.yearID = b.yearID AND b.playerID = f.playerID AND b.playerID = t.playerID GROUP BY b.playerID";
 			
-			rows  = sqlState.executeQuery(selectStuff);
 			
-			// int numOfCol;
+			// A ResultSet contains a table of data representing the result
+			// of the query. It can not be changed and can only be read in the direction
 			
-			// metaData = rows.getMetaData();
+		
+			int numOfCol;
+			 
+			// Retrieves the number, types and properties of the Query Results
+			 
+			metaData = rows.getMetaData();
+			 
 			
-			// numOfCol = metaData.getColumnCount();
 			
 			Object[] tempRow;
 			
