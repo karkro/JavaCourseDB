b66304d63c8fd2e5d2d21e24b3c834d6d4d8d059
karkro <karkro@oem-laptop>
2019-03-03 22:05:09 +0100

    Servlet - start

---
+++ JavaCourseDB/src/JavaLesson39.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.event.HyperlinkListener;
+
+public class JavaLesson39 extends JFrame implements HyperlinkListener, ActionListener {
+
+	public static void main(String[] args) {
+		new JavaLesson39("file:///var/www/html/Applet01")
+	}
+}
